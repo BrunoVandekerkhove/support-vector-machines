0a8cebd484110ee0d1098e52178c2395de3af2b0
Bruno Vandekerkhove <brunovandekerkhove@protonmail.com>
2020-05-20 00:11:19 +0200

    Refactor export_pdf (session_3)

--- src/session_3.m
+++ src/session_3.m
@@ -10,3 +10,18 @@ close all
 kpca_script
 %% 1.2 Spectral clustering
 sclustering_script
+%% 1.3a Fixed-size LS-SVM
+fixedsize_script1
+%% 1.3b Fixed-size LS-SVM
+fslssvm_script
+%% Functions
+function export_pdf(h, output_name)
+%EXPORT_PDF Exports the given figure to a pdf file.
+    set(h, 'PaperUnits','centimeters');
+    set(h, 'Units','centimeters');
+    pos=get(h,'Position');
+    set(h, 'PaperSize', [pos(3) pos(4)]);
+    set(h, 'PaperPositionMode', 'manual');
+    set(h, 'PaperPosition',[0 0 pos(3) pos(4)]);
+    print('-dpdf', strcat('figures/', output_name));
+end
