3e4d77457205b7f3b6b142283e26a5c670fb902c
Bruno Vandekerkhove <brunovandekerkhove@protonmail.com>
2020-05-20 00:11:15 +0200

    Refactor export_pdf (session_2)

--- src/session_2.m
+++ src/session_2.m
@@ -151,3 +151,14 @@ for wFun = wFuns
     set(p1, 'Color', [0, 0.4470, 0.7410]);
     export_pdf(gcf, sprintf('estimation/robustregression_%s_alpha', wFun))
 end
+%% Functions
+function export_pdf(h, output_name)
+%EXPORT_PDF Exports the given figure to a pdf file.
+    set(h, 'PaperUnits','centimeters');
+    set(h, 'Units','centimeters');
+    pos=get(h,'Position');
+    set(h, 'PaperSize', [pos(3) pos(4)]);
+    set(h, 'PaperPositionMode', 'manual');
+    set(h, 'PaperPosition',[0 0 pos(3) pos(4)]);
+    print('-dpdf', strcat('figures/', output_name));
+end
