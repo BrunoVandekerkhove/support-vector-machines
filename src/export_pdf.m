2f61aef21b2a2f545e71cdb208c5c1cc17e373e9
Bruno Vandekerkhove <brunovandekerkhove@protonmail.com>
2020-05-20 18:25:57 +0200

    Create export_pdf.m

---
+++ src/export_pdf.m
@@ -0,0 +1,10 @@
+function export_pdf(h, output_name)
+%EXPORT_PDF Exports the given figure to a pdf file.
+    set(h, 'PaperUnits','centimeters');
+    set(h, 'Units','centimeters');
+    pos=get(h,'Position');
+    set(h, 'PaperSize', [pos(3) pos(4)]);
+    set(h, 'PaperPositionMode', 'manual');
+    set(h, 'PaperPosition',[0 0 pos(3) pos(4)]);
+    print('-dpdf', strcat('figures/', output_name));
+end
