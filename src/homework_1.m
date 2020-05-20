656969dc0ae4686e37d3c13dee7583867ca23274
Bruno Vandekerkhove <brunovandekerkhove@protonmail.com>
2020-05-21 00:23:52 +0200

    Wisconsin breast cancer dataset

--- src/homework_1.m
+++ src/homework_1.m
@@ -10,14 +10,12 @@ close all
 load('ripley')
 visualise(Xtrain, Ytrain)%, Xtest, Ytest)
 export_pdf(gcf, 'classification/ripley')
-for kernel = ["RBF_kernel"]
+for kernel = ["lin_kernel", "RBF_kernel"]
     % Tuning
     tune_params = {Xtrain, Ytrain, 'c', 0.1, [], kernel};
     [gam,sig2,cost] = tunelssvm(tune_params, 'simplex', 'crossvalidatelssvm', {10, 'misclass'});
     fprintf('Tuning results (g,s,c) = (%.3f,%.3f,%.3f)\n', gam, sig2, cost)
     % Training
-    gam = 1.0;
-    sig2 = 5.0;
     parameters = {Xtrain, Ytrain, 'c', gam, sig2, kernel};
     [alpha,b] = trainlssvm(parameters);
     fprintf('Number of support vectors = %d\n', length(alpha));
@@ -69,27 +67,63 @@ for kernel = ["sigmoid", "spline", "bspline","linear", "poly", "rbf"]%, "fourier
     %   sgn function to predict
     error = svcerror(Xtrain, Ytrain, Xtest, Ytest, kernel, alpha, bias);
     fprintf('Error for kernel %s = %.5f\n', kernel, error/length(Xtest));
-    export_pdf(gcf, sprintf('classification/ripley_svm_%s', kernel))
+    %export_pdf(gcf, sprintf('classification/ripley_svm_%s', kernel))
     waitforbuttonpress;
 end
 %% 2.2 Breast Cancer dataset
 load('breast')
-visualise(trainset, labels_train)
+Xtrain = trainset;
+Ytrain = labels_train;
+Xtest = testset;
+Ytest = labels_test;
+%visualise(trainset, labels_train, 1)
 %export_pdf(gcf, 'classification/wisconsin')
+for kernel = ["lin_kernel", "poly_kernel", "RBF_kernel"]
+    % Tuning
+    tune_params = {Xtrain, Ytrain, 'c', 0.1, [], kernel};
+    if kernel == "poly_kernel"
+        [gam,tau,degree,cost] = tunelssvm(tune_params, 'simplex', 'crossvalidatelssvm', {10, 'misclass'});
+        fprintf('Tuning results (g,t,d,c) = (%.3f,%.3f,%.3f,%.3f)\n', gam, tau, degree, cost)
+        parameters = {Xtrain, Ytrain, 'c', gam, [tau,degree], kernel};
+    else
+        [gam,sig2,cost] = tunelssvm(tune_params, 'simplex', 'crossvalidatelssvm', {10, 'misclass'});
+        fprintf('Tuning results (g,s,c) = (%.3f,%.3f,%.3f)\n', gam, sig2, cost)
+        parameters = {Xtrain, Ytrain, 'c', gam, sig2, kernel};
+    end
+    % Training
+    %parameters = {Xtrain, Ytrain, 'c', gam, sig2, kernel};
+    [alpha,b] = trainlssvm(parameters);
+    fprintf('Number of support vectors = %d\n', length(alpha));
+    % Testing
+    [test_classes, Ylatent] = simlssvm(parameters, {alpha,b}, Xtest);
+    miss_rate = sum(test_classes ~= Ytest) / length(Ytest);
+    fprintf('Missclassification rate : %.5f\n', miss_rate)
+    % ROC
+    [auc,~,thresholds,fpr,tpr] = roc(Ylatent, Ytest);
+    %export_pdf(gcf, sprintf('classification/ripley_roc_%s', kernel))
+    fprintf('ROC_AUC : %.5f\n', auc)
+end
 %% 2.3 Diabetes dataset
 load('diabetes')
 visualise(trainset, labels_train)
 %export_pdf(gcf, 'classification/diabetes')
 %% Functions
-function visualise(X, y, Xt, yt)
+function visualise(X, y, use_pca)
+    if nargin < 3; use_pca = 0; end
+    dim = size(X,2);
+    if dim > 2
+        if use_pca
+            [~, score] = pca(X.');
+            X = score(:, 1:2);
+            size(X)
+        else
+            X = tsne(X);
+        end
+    end
     figure
     hold on
     plot(X(y>0,1), X(y>0,2), 'ko','MarkerFaceColor','b');
     plot(X(y<0,1), X(y<0,2), 'ko','MarkerFaceColor','r');
-    if nargin > 2
-        plot(Xt(yt>0,1), Xt(yt>0,2), 'bo','MarkerFaceColor','b');
-        plot(Xt(yt<0,1), Xt(yt<0,2), 'ro','MarkerFaceColor','r');
-    end
     hold off
 end
 function export_pdf(h, output_name)
