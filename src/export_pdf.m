64c74ae459602b5ab48f5e8129085d92679ad506
Bruno Vandekerkhove <brunovandekerkhove@protonmail.com>
2020-05-24 12:08:08 +0200

    Update export_pdf.m

--- src/export_pdf.m
+++ src/export_pdf.m
@@ -1,8 +1,11 @@
-function export_pdf(h, output_name)
+function export_pdf(h, output_name, width, height)
 %EXPORT_PDF Exports the given figure to a pdf file.
+    if nargin > 2
+        set(h, 'Position', [0 0 width height])
+    end
     set(h, 'PaperUnits','centimeters');
     set(h, 'Units','centimeters');
-    pos=get(h,'Position');
+    pos = get(h,'Position');
     set(h, 'PaperSize', [pos(3) pos(4)]);
     set(h, 'PaperPositionMode', 'manual');
     set(h, 'PaperPosition',[0 0 pos(3) pos(4)]);
